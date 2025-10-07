6ebd3a1d571c6c3130fbfebc2bf68f5dac029673
Abolfazl <abolfazlmnf83@gmail.com>
2025-10-07 15:17:42 -0700

    edit folders

--- src/App/router/router.jsx
+++ src/App/router/router.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import HomePage from '../../components/pages/HomePage'
+import HomePage from '../../pages/HomePage'
 
 const router = createBrowserRouter([
     { path: '/', element: <HomePage /> }
