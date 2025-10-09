57d05d6796e7fad007a009e59f51d6095918ad64
reza <rezakazemi1381@yahoo.com>
2025-10-09 15:06:26 +0330

    Update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import style from "./Navbar.module.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={` ${style.main} flex items-center justify-between `}>
+      <div className={` ${style.left}  `}>
+        <button className={` ${style.button} rounded-4xl font-bold `}>
+          ورود / ثبت نام
+        </button>
+
+        <div className={` ${style.searchcontainer}  `}>
+          <input
+            type="text"
+            className={` ${style.searchinput}  `}
+            placeholder="    جستجو کنید..."
+          />
+          <button className={` ${style.searchbutton}  `}>
+            <SearchIcon sx={{ color: "white", fontSize: 35 }} />
+          </button>
+        </div>
+      </div>
+      <ul className={` ${style.ul} flex items-center justify-around `}>
+        <li className={`  ${style.li} text-lg cursor-pointer font-semibold`}>
+          {" "}
+          اخبار
+        </li>
+        <li className={`  ${style.li} text-lg cursor-pointer font-semibold`}>
+          دوره ها
+        </li>
+        <li className={`  ${style.li} text-lg cursor-pointer font-semibold`}>
+          صفحه اصلی
+        </li>
+        <li className="text-xl font-bold">آکادمی بحر</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import HeroSection from '../../components/HeroSection/HeroSection'
+import React from "react";
+import HeroSection from "../../components/HeroSection/HeroSection";
 
 const Landing = () => {
-  return (
-    <div>
-        <HeroSection/>
-    </div>
-  )
-}
+  return <div>{/* <HeroSection/> */}</div>;
+};
 
-export default Landing
+export default Landing;
