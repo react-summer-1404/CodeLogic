2a01fe3773eb5ab9b3c93f83b69e138ad9f111e1
reza <rezakazemi1381@yahoo.com>
2025-10-10 14:38:23 +0330

    Footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import style from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={` ${style.main} `}>
+      <div className={` ${style.top} `}></div>
+      <div className={` ${style.middle} `}></div>
+      <div className={` ${style.bottom} `}></div>
+    </div>
+  );
 };
 
 export default Footer;
