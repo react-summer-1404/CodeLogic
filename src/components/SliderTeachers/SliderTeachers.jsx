766750c7fbe3bfd213d53b423f15bcb6e97b6375
Parsa <mparsabahredar@gmail.com>
2025-10-12 23:36:28 +0330

    Landing

--- src/components/SliderTeachers/SliderTeachers.jsx
+++ src/components/SliderTeachers/SliderTeachers.jsx
@@ -23,10 +23,12 @@ const SliderTeachers = () => {
   const {t} = useTranslation();
 
   return (
-    <div>
+    <div className='flex flex-col items-center gap-8 w-full'>
       <div className='flex flex-col items-center gap-2 pt-[163px]'>
-        <h2 className='font-bold text-[32px] text-[#008C78]'>{t('آشنایی با اساتید حرفه ای ما')}</h2>
-        <p>{t('یادگیری از برترین مدرسین با تجربه و دانش به‌روز')}</p>
+        <h2 className='font-bold text-[#008C78]
+        sa:text-[24px]   sm:text-[28px]   lg:text-[32px]'>{t('آشنایی با اساتید حرفه ای ما')}</h2>
+        <p className='font-regular
+        sa:text-sm   sm:text-lg   lg:text-2xl'>{t('یادگیری از برترین مدرسین با تجربه و دانش به‌روز')}</p>
       </div>
       <ButtonsSeeMore seeAllText={'مشاهده همه استادها'} sliderRef={sliderRef}/>
       <div className='flex flex-nowrap gap-8 overflow-hidden w-full pt-10 px-10 scroll-smooth scrollbar-hide' dir='ltr' ref={sliderRef}>
