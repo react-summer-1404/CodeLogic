d1754cab46685bfc09f71c0667f572a8f38f0e0d
reza <rezakazemi1381@yahoo.com>
2025-10-17 02:36:28 +0330

    Update navbar

--- src/components/useTheme/useTheme.jsx
+++ src/components/useTheme/useTheme.jsx
@@ -24,34 +24,3 @@ export function useTheme() {
 
   return { theme, toggleTheme };
 }
-// src/hooks/useTheme.js
-// import { useEffect, useState } from "react";
-
-// export function useTheme() {
-//   const [theme, setTheme] = useState(() => {
-//     // اول از localStorage بخونه، در غیر اینصورت از تنظیمات سیستم استفاده کنه
-//     const savedTheme = localStorage.getItem("theme");
-//     if (savedTheme) return savedTheme;
-//     return window.matchMedia("(prefers-color-scheme: dark)").matches
-//       ? "dark"
-//       : "light";
-//   });
-
-//   useEffect(() => {
-//     const html = document.documentElement;
-
-//     if (theme === "dark") {
-//       html.classList.add("dark");
-//     } else {
-//       html.classList.remove("dark");
-//     }
-
-//     localStorage.setItem("theme", theme);
-//   }, [theme]);
-
-//   const toggleTheme = () => {
-//     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
-//   };
-
-//   return { theme, toggleTheme };
-// }
