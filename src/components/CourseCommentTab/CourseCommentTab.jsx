91bbf25e6825458392bb17e9a00b7b9dad34233d
Parsa <mparsabahredar@gmail.com>
2025-10-19 13:12:30 +0330

    commit

--- src/components/CourseCommentTab/CourseCommentTab.jsx
+++ src/components/CourseCommentTab/CourseCommentTab.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 
-const CourseCommentTab = () => {
+const CourseCommentTab = ({activeTab, setActiveTab}) => {
 
-    const {t} = useTranslation();
+  const {t} = useTranslation();
 
-    const [toggleTab, setToggleTab] = useState(false);
+  const handleTabChange = (tabName) => {
+    setActiveTab(tabName);
+  }
 
   return (
     <div className='flex gap-2 pt-10'>
-      <button onClick={() => setToggleTab(!toggleTab)} className={`py-[9px] px-4 rounded-[48px] cursor-pointer
-      ${toggleTab ? '' : 'text-[#FFFFFF] bg-[#008C78]'}`}>
-          {t('مشخصات دوره')}
+      <button onClick={() => setActiveTab('detail')} className={`py-[9px] px-4 rounded-[48px] cursor-pointer
+      ${activeTab === 'detail' ? 'text-[#FFFFFF] bg-[#008C78]' : ''}`}>
+        {t('مشخصات دوره')}
       </button>
-      <button onClick={() => {setToggleTab(!toggleTab)}} className={`py-[9px] px-4 rounded-[48px] cursor-pointer
-      ${toggleTab ? 'text-[#FFFFFF] bg-[#008C78]' : ''}`}>
-          {t('نظرات کاربران')}
+      <button onClick={() => {setActiveTab('comment')}} className={`py-[9px] px-4 rounded-[48px] cursor-pointer
+      ${activeTab === 'comment' ? 'text-[#FFFFFF] bg-[#008C78]' : ''}`}>
+        {t('نظرات کاربران')}
       </button>
     </div>
   )
--- src/components/CourseDetailMain/CourseDetailMain.jsx
+++ src/components/CourseDetailMain/CourseDetailMain.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import ImageInfo from '../ImageInfo/ImageInfo'
 import CourseCommentTab from '../CourseCommentTab/CourseCommentTab'
@@ -9,15 +9,29 @@ import CourseComments from '../CourseComments/CourseComments'
 
 const CourseDetailMain = () => {
 
-    const {t} = useTranslation();
+  const {t} = useTranslation();
+
+  const [activeTab, setActiveTab] = useState('detail');
 
   return (
     <div className='flex flex-col gap-4'>
-        <ImageInfo/>
-        <CourseCommentTab/>
-        <CourseDetailSection/>
-        <CourseDesc/> 
-        <CourseComments/>
+      <ImageInfo/>
+      <CourseCommentTab activeTab={activeTab} setActiveTab={setActiveTab}/>
+      {
+        activeTab === 'detail' && (
+          <>
+            <CourseDetailSection/>
+            <CourseDesc/> 
+          </>
+        )
+      }
+      {
+        activeTab === 'comment' && (
+          <>
+            <CourseComments/> 
+          </>
+        )
+      }
     </div>
   )
 }
