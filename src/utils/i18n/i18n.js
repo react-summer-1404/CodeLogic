ccbe68da2d34ed7d08b0c8009606f27010e98a68
Parsa <mparsabahredar@gmail.com>
2025-10-21 20:33:51 +0330

    Commit

--- src/utils/i18n/i18n.js
+++ src/utils/i18n/i18n.js
@@ -1,8 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
-import faTranslation from "../utils/fa/fa.json";
-import enTranslation from "../utils/en/en.json";
+import faTranslation from "./fa/fa.json";
+import enTranslation from "./en/en.json";
 
 i18n.use(initReactI18next).init({
   resources: {
