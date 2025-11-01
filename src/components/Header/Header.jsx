68ce906737d1ad03a51af7743076ce0cc5714a55
reza <rezakazemi1381@yahoo.com>
2025-11-02 02:25:48 +0330

    Bug fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,6 @@ import TranslateButton from "../TranslateButton/TranslateButton";
 import { useTheme } from "../../utils/hooks/useTheme/useTheme";
 
 const Header = () => {
-  
   const { t, i18n } = useTranslation();
   const isRtl = i18n.language === "fa";
 
@@ -22,35 +21,38 @@ const Header = () => {
         ${isDark ? "bg-[#1e1e1e]" : "bg-[#eefffc]"}`}
     >
       <div
-        className="block sm:hidden cursor-pointer"
+        className="block md:hidden cursor-pointer"
         onClick={() => setMobileMenu(!mobileMenu)}
       >
         <MenuIcon sx={{ color: isDark ? "white" : "black", fontSize: 28 }} />
       </div>
 
       <ul className="hidden sm:hidden md:flex flex-row items-center gap-6">
-        <li 
+        <li
           className={` cursor-pointer text-sm md:text-base lg:text-lg font-bold transition-colors duration-200 hover:text-[#008c78] ${
             isDark ? "text-white hover:text-[#00bfa5]" : ""
           }`}
         >
           {t("navbar.academy")}
         </li>
-        <Link to={'/'}
+        <Link
+          to={"/"}
           className={` cursor-pointer text-sm md:text-base lg:text-lg font-semibold transition-colors duration-200 hover:text-[#008c78] ${
             isDark ? "text-white hover:text-[#00bfa5]" : ""
           }`}
         >
           {t("navbar.home")}
         </Link>
-        <Link to={'/courselist'}
+        <Link
+          to={"/courselist"}
           className={` cursor-pointer  text-sm md:text-base lg:text-lg font-semibold transition-colors duration-200 hover:text-[#008c78] ${
             isDark ? "text-white hover:text-[#00bfa5]" : ""
           }`}
         >
           {t("navbar.courses")}
         </Link>
-        <Link to={'/newslist'}
+        <Link
+          to={"/news"}
           className={` cursor-pointer text-sm md:text-base lg:text-lg font-semibold transition-colors duration-200 hover:text-[#008c78] ${
             isDark ? "text-white hover:text-[#00bfa5]" : ""
           }`}
@@ -61,7 +63,7 @@ const Header = () => {
 
       <div className="flex items-center gap-2">
         <div
-          className={`relative flex items-center bg-white dark:bg-[#2a2a2a] rounded-[28px] transition-shadow duration-300 ${
+          className={`hidden md:flex relative items-center bg-white dark:bg-[#2a2a2a] rounded-[28px] transition-shadow duration-300 ${
             isRtl ? "flex-row-reverse" : "flex-row-reverse"
           }`}
         >
@@ -97,20 +99,39 @@ const Header = () => {
           {isDark ? "☀️" : "🌙"}
         </button>
         <TranslateButton />
-        <Link to={'/RegisterStepOne'}>
-          <button className=" cursor-pointer  bg-[#008c78] text-white font-bold rounded-full px-3 sm:px-4 md:px-6 py-1 sm:py-2 md:py-3 text-xs sm:text-sm md:text-base transition-colors duration-300 hover:bg-[#007563]">
+        <Link to={"/RegisterStepOne"}>
+          <button className=" cursor-pointer  bg-[#008c78] text-white font-bold rounded-full px-2 sm:px-4 md:px-6 py-1 sm:py-2 md:py-3 text-xs sm:text-sm md:text-base transition-colors duration-300 hover:bg-[#007563]">
             {t("navbar.login")}
           </button>
         </Link>
       </div>
 
-
-
-
       {mobileMenu && (
         <ul
-          className={` !bg-[#008c78] text-white fixed top-0 right-0 h-full w-1/3 sm:w-1/2 bg-white dark:bg-[#1e1e1e] shadow-lg p-4 flex flex-col gap-4 z-50`}
+          className={`!bg-[#008c78] text-white fixed top-0 right-0 h-full w-1/3 sm:w-1/2 bg-white dark:bg-[#1e1e1e] shadow-lg p-4 flex flex-col gap-4 z-50`}
         >
+          <div
+            className={`relative flex items-center bg-white dark:bg-[#2a2a2a] rounded-[28px] transition-shadow duration-300 mt-2 ${
+              isRtl ? "flex-row-reverse" : "flex-row-reverse"
+            }`}
+          >
+            <input
+              type="text"
+              className={`font-medium text-[#7e7e7e] dark:text-white bg-transparent rounded-[28px] px-2 sm:px-3 py-1 sm:py-2 text-xs sm:text-sm md:text-base outline-none flex-grow transition-all duration-300`}
+              placeholder={t("navbar.search_placeholder")}
+            />
+
+            <button
+              className="absolute top-1/2 transform -translate-y-1/2 dark:bg-[#008c78] bg-[#008c78] p-2 rounded-full cursor-pointer transition-colors duration-300 hover:bg-[#007563]"
+              style={{
+                [isRtl ? "left" : "right"]: "1px",
+                [isRtl ? "right" : "left"]: "auto",
+              }}
+            >
+              <SearchIcon sx={{ color: "white", fontSize: 24 }} />
+            </button>
+          </div>
+
           <li
             className={`text-sm font-bold transition-colors duration-200 hover:text-[#008c78] ${
               isDark ? "text-white hover:text-[#00bfa5]" : ""
@@ -118,21 +139,24 @@ const Header = () => {
           >
             {t("navbar.academy")}
           </li>
-          <Link to={'/'}
+          <Link
+            to={"/"}
             className={`text-sm font-semibold transition-colors duration-200 hover:text-[#008c78] ${
               isDark ? "text-white hover:text-[#00bfa5]" : ""
             }`}
           >
             {t("navbar.home")}
           </Link>
-          <Link to={'/courselist'}
+          <Link
+            to={"/courselist"}
             className={`text-sm font-semibold transition-colors duration-200 hover:text-[#008c78] ${
               isDark ? "text-white hover:text-[#00bfa5]" : ""
             }`}
           >
             {t("navbar.courses")}
           </Link>
-          <Link to={'/newslist'}
+          <Link
+            to={"/newslist"}
             className={`text-sm font-semibold transition-colors duration-200 hover:text-[#008c78] ${
               isDark ? "text-white hover:text-[#00bfa5]" : ""
             }`}
