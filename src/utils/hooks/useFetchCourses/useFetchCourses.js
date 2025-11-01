f15a72563069734812d2a0d50ddc6158d89cb33f
Parsa <mparsabahredar@gmail.com>
2025-11-01 11:11:04 +0330

    Commit

--- src/utils/hooks/useFetchCourses/useFetchCourses.js
+++ src/utils/hooks/useFetchCourses/useFetchCourses.js
@@ -1,40 +1,40 @@
-// import { useState, useEffect, useMemo } from 'react';
-// import apiClient from '../../../core/interceptor/interceptor';
+import { useState, useEffect, useMemo } from 'react';
+import apiClient from '../../../core/interceptor/interceptor';
 
 
-// const useFetchCourses = (url, pageNumber = 1, pageSize = 10) => {
-//   const [courses, setCourses] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [totalRecords, setTotalRecords] = useState(0); 
+const useFetchCourses = (url, pageNumber = 1, pageSize = 10) => {
+  const [courses, setCourses] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalRecords, setTotalRecords] = useState(0); 
   
-//   const finalUrl = useMemo(() => {
-//     const separator = url.includes('?') ? '&' : '?';
-//     return `${url}${separator}PageNumber=${pageNumber}&RowsOfPage=${pageSize}`;
-//   }, [url, pageNumber, pageSize]); 
+  const finalUrl = useMemo(() => {
+    const separator = url.includes('?') ? '&' : '?';
+    return `${url}${separator}PageNumber=${pageNumber}&RowsOfPage=${pageSize}`;
+  }, [url, pageNumber, pageSize]); 
 
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       if(!url) return;
-//       setIsLoading(true);
-//       try{
-//         const response = await apiClient.get(finalUrl); 
-//         setCourses(response.courseFilterDtos || []);
-//         setTotalRecords(response.totalCount || 0); 
-//       }
-//       catch(error){
-//         setCourses([]);
-//         setTotalRecords(0); 
-//       }
-//       finally{
-//         setIsLoading(false);
-//       }
-//     };
+  useEffect(() => {
+    const fetchData = async () => {
+      if(!url) return;
+      setIsLoading(true);
+      try{
+        const response = await apiClient.get(finalUrl); 
+        setCourses(response.courseFilterDtos || []);
+        setTotalRecords(response.totalCount || 0); 
+      }
+      catch(error){
+        setCourses([]);
+        setTotalRecords(0); 
+      }
+      finally{
+        setIsLoading(false);
+      }
+    };
     
-//     fetchData();
-//   }, [finalUrl]); 
+    fetchData();
+  }, [finalUrl]); 
 
-//   return {courses, totalRecords, isLoading};
-// };
+  return {courses, totalRecords, isLoading};
+};
 
-// export default useFetchCourses;
+export default useFetchCourses;
