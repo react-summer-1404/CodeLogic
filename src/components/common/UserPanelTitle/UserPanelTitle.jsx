da3702205247a02102e0a8c81849766be03a06ac
Parsa <mparsabahredar@gmail.com>
2025-11-02 14:47:44 +0330

    Commit

---
+++ src/components/common/UserPanelTitle/UserPanelTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserPanelTitle = () => {
+  return (
+    <div className='flex items-center'>
+        
+    </div>
+  )
+}
+
+export default UserPanelTitle
--- src/pages/UserPanel/MyCourses/MyCourses.jsx
+++ src/pages/UserPanel/MyCourses/MyCourses.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import UserPanelSearch from '../../../components/common/UserPanelSearch/UserPanelSearch'
 import UserPanelFilter from '../../../components/common/UserPanelFilter/UserPanelFilter'
+import UserPanelTitle from '../../../components/common/UserPanelTitle/UserPanelTitle' 
+import { useTranslation } from 'react-i18next'
 
 
 
 const MyCourses = () => {
+
+  const {t} = useTranslation();
 
   return (
     <div className='h-[85%]'>
@@ -12,6 +16,15 @@ const MyCourses = () => {
         <UserPanelSearch width='439px'/>  
         <UserPanelFilter/>
       </div>
+      <UserPanelTitle 
+      titleData={{
+        title1: t('myCourses.title1'),
+        title2: t('myCourses.title2'),
+        title3: t('myCourses.title3'),
+        title4: t('myCourses.title4'),
+        title5: t('myCourses.title5'),
+        title6: t('myCourses.title6')
+      }}/>
     </div>
   )
 }
