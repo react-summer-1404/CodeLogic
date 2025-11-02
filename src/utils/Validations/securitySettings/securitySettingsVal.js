40f5e571890ec81810ec7b0297e6e902ac93b92d
Abolfazl <abolfazlmnf83@gmail.com>
2025-11-03 00:53:41 +0330

    bind securitySetting to api

--- src/utils/Validations/securitySettings/securitySettingsVal.js
+++ src/utils/Validations/securitySettings/securitySettingsVal.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import i18n from '../../i18n/i18n';
 export const securitySettingsVal = () => {
     return Yup.object({
-        currentPassword: Yup.string().required(() => i18n.t('loginVal.requiredPass')),
+        oldPassword: Yup.string().required(() => i18n.t('loginVal.requiredPass')),
         newPassword: Yup.string()
             .required(() => i18n.t('loginVal.requiredPass'))
             .matches(/\d/, () => i18n.t('securitySetting.minNumbers')),
