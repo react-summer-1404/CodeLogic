0803571e2307b804b0cb95fc63d2eba713476d61
Parsa <mparsabahredar@gmail.com>
2025-11-08 02:07:33 +0330

    Commit

--- src/pages/UserPanel/MyNewsComments/MyNewsComments.jsx
+++ src/pages/UserPanel/MyNewsComments/MyNewsComments.jsx
@@ -5,6 +5,7 @@ import UserPanelTitle from '../../../components/common/userPanel/UserPanelTitle/
 import UserPanelShowNumber from '../../../components/common/userPanel/UserPanelShowNumber/UserPanelShowNumber'
 import ReactPaginate from 'react-paginate'
 import { useTranslation } from 'react-i18next'
+import MyNewsComment from '../../../components/common/userPanel/MyNewsComment/MyNewsComment'
 
 
 
@@ -18,36 +19,39 @@ const MyNewsComments = () => {
         <UserPanelSearch width={'w-[439px]'}/>  
         <UserPanelFilter/>
       </div>
-      <div className='h-[440px] p-6 bg-[#FFFFFF] rounded-2xl   dark:bg-[#454545]'>
-        <UserPanelTitle 
-        titleData={{
-          title1: t('myNewsComments.title1'), 
-          title2: t('myNewsComments.title2'), pr2: 'pr-[112px]',
-          title3: t('myNewsComments.title3'), pr3: 'pr-20',
-          title4: t('myNewsComments.title4'), pr4: 'pr-[168px]',
-          title5: t('myNewsComments.title5'), pr5: 'pr-[104px]',
-          title6: t('myNewsComments.title6'), pr6: 'pr-20',
-        }}/>
-      </div>
-      <div className='flex justify-between items-center'>
-        <ReactPaginate
-          breakLabel="..."
-          nextLabel=" >"
-          previousLabel="< "
-          // onPageChange={handlePageChange}
-          // pageCount={pageSize}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={3}
-          // forcePage={currentPage}
-          containerClassName="flex flex-wrap justify-center gap-1 sm:gap-2"
-          pageClassName="px-3 py-2 sm:px-5 sm:py-3 rounded-[15px] font-semibold shadow-md cursor-pointer text-sm sm:text-xl bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
-          activeClassName="!bg-[#008C78] text-white rounded-2xl shadow-md"
-          previousClassName="px-2 py-1 sm:px-3 sm:py-1 rounded-2xl shadow-md cursor-pointer text-sm bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
-          nextClassName="px-2 py-1 sm:px-3 sm:py-1 rounded-2xl shadow-md cursor-pointer text-sm bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
-          previousLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
-          nextLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
-        />
-        <UserPanelShowNumber/>
+      <div className='flex flex-col justify-between h-[440px] p-6 bg-[#FFFFFF] rounded-2xl   dark:bg-[#454545]'>
+        <div className='flex flex-col gap-4'>
+          <UserPanelTitle 
+          titleData={{
+            title1: t('myNewsComments.title1'), justify1: 'justify-start', w1: 'w-60',
+            title2: t('myNewsComments.title2'), justify2: 'justify-start', w2: 'w-52',
+            title3: t('myNewsComments.title3'), justify3: 'justify-start', w3: 'w-52',
+            title4: t('myNewsComments.title4'), justify4: 'justify-center', w4: 'w-28',
+            title5: t('myNewsComments.title5'), justify5: 'justify-center', w5: 'w-30',
+            title6: t('myNewsComments.title6'), justify6: 'justify-center', w6: 'w-24',
+          }}/>
+          <MyNewsComment/>
+        </div>
+        <div className='flex justify-between items-center'>
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=" >"
+            previousLabel="< "
+            // onPageChange={handlePageChange}
+            // pageCount={pageSize}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={3}
+            // forcePage={currentPage}
+            containerClassName="flex flex-wrap justify-center gap-1 sm:gap-2"
+            pageClassName="px-3 py-2 sm:px-5 sm:py-3 rounded-[15px] font-semibold shadow-md cursor-pointer text-sm sm:text-xl bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
+            activeClassName="!bg-[#008C78] text-white rounded-2xl shadow-md"
+            previousClassName="px-2 py-1 sm:px-3 sm:py-1 rounded-2xl shadow-md cursor-pointer text-sm bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
+            nextClassName="px-2 py-1 sm:px-3 sm:py-1 rounded-2xl shadow-md cursor-pointer text-sm bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
+            previousLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
+            nextLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
+          />
+          <UserPanelShowNumber/>
+        </div>
       </div>
     </div>
   )
