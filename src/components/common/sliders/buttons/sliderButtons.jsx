5501c1f4b37f2de1bce4c325030218030f3340b6
Parsa <mparsabahredar@gmail.com>
2025-11-11 13:32:23 +0330

    C

--- src/components/common/sliders/buttons/sliderButtons.jsx
+++ src/components/common/sliders/buttons/sliderButtons.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import leftIcon from '../../../../assets/Icons/A/left.png';
 import rightIcon from '../../../../assets/Icons/A/right.png';
+
+
 const SliderButtons = ({ sliderRef }) => {
+    
     const scroll = (direction = 'left' | 'right') => {
         if (sliderRef.current) {
-            console.log(sliderRef);
             sliderRef.current.scrollBy({
                 left: direction === 'left' ? -150 : 150,
                 behavior: 'smooth',
             });
         }
     };
+
+    const buttonClassName = 
+    'cursor-pointer shadow-lg text-center w-[45px] h-[45px] rounded-full bg-[#008C78] text-[#1E1E1E] bg-[center_center] bg-no-repeat   dark:bg-[#ffff] dark:text-[#ffff]'
+
     return (
         <div
-            className="flex justify-between gap-5 visible md:invisible mx-auto mt-10 md:m-0 w-full "
-            style={{ direction: 'ltr' }}
-        >
+        className="flex justify-between gap-5 visible md:invisible mx-auto mt-10 md:m-0 w-full "
+        style={{ direction: 'ltr' }}>
             <button
-                onClick={() => scroll('left')}
-                className='cursor-pointer shadow-lg text-center w-[45px] h-[45px] rounded-full
-             bg-[#008C78]  dark:bg-[#ffff] dark:text-[#ffff] text-[#1E1E1E]"}  bg-[center_center]
-              bg-no-repeat '
-                style={{ backgroundImage: `url(${leftIcon})` }}
+            onClick={() => scroll('left')}
+            style={{ backgroundImage: `url(${leftIcon})` }}
+            className={buttonClassName}
             ></button>
             <button
-                onClick={() => scroll('right')}
-                className='cursor-pointer shadow-lg text-center w-[45px] h-[45px] rounded-full  bg-[#008C78] dark:bg-[#ffff] 
-             dark:text-[#ffff] text-[#1E1E1E]"} bg-[center_center] bg-no-repeat '
-                style={{ backgroundImage: `url(${rightIcon})` }}
+            onClick={() => scroll('right')}
+            style={{ backgroundImage: `url(${rightIcon})` }}
+            className={buttonClassName}
             ></button>
         </div>
     );
