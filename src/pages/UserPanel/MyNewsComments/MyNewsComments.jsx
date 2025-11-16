00ee9fb51f0da37bb2052c1a59315e2e81a3f86d
Parsa <mparsabahredar@gmail.com>
2025-11-16 20:53:30 +0330

    C

--- src/pages/UserPanel/MyNewsComments/MyNewsComments.jsx
+++ src/pages/UserPanel/MyNewsComments/MyNewsComments.jsx
@@ -1,72 +1,141 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import UserPanelSearch from '../../../components/common/UserPanelSearch/UserPanelSearch'
-import UserPanelFilter from '../../../components/common/UserPanelFilter/UserPanelFilter'
 import UserPanelTitle from '../../../components/common/UserPanelTitle/UserPanelTitle'
-import UserPanelShowNumber from '../../../components/common/UserPanelShowNumber/UserPanelShowNumber'
 import MyNewsComment from '../../../components/userPanel/MyNewsComment/MyNewsComment'
 import myNewsComments from '../../../core/services/api/get/myNewsComments'
 import ReactPaginate from 'react-paginate'
 import { t } from 'i18next'
-
+import Lottie from "lottie-react"
+import empty from "../../../assets/Images/empty.json"
+import FavoritesSkeleton from "../../../components/common/skeleton/favorites/FavoritesSkeleton"
 
 
 const MyNewsComments = () => {
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [commentsPerPage, setCommentsPerPage] = useState(2);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filterOption, setFilterOption] = useState('all');
 
-  const [searchQuery , setSearchQuery] = useState('');
   const handleSearch = (searchTerm) => {
     setSearchQuery(searchTerm);
+    setCurrentPage(1);
   };
 
 
   const { data: myNewsCommentsData, isLoading } = useQuery({
     queryKey: ['MYNEWSCOMMENTS', searchQuery],
     queryFn: () => myNewsComments({
-      Query: searchQuery
-    })  
-  })
+      Query: searchQuery,
+      SortType: 'accept'
+    })
+  });
+
+
+
+  const allComments = myNewsCommentsData?.myNewsCommetDtos || [];
+  const filteredComments = allComments.filter((comment) => {
+    const newsTitle = comment?.newsName ?? "";
+    const matchesTitle = newsTitle
+      .toLowerCase()
+      .trim()
+      .includes(searchQuery.trim().toLowerCase());
+    if (filterOption === "all" || filterOption === "همه") {
+      return matchesTitle;
+    }
+    const bool = filterOption === "true";
+    const matchesFilters = comment.accept === bool;
+    return matchesFilters && matchesTitle;
+  });
+
+
+  const startIndex = (currentPage - 1) * commentsPerPage;
+  const endIndex = startIndex + commentsPerPage;
+  const currentComments = filteredComments.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(filteredComments.length / commentsPerPage);
+
+  const handlePageChange = (p) => {
+    const selectedPage = p.selected + 1;
+    setCurrentPage(selectedPage);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
 
   return (
-    <div className='flex flex-col gap-10 h-[84%] mt-4 p-8 bg-[#F3F4F6] rounded-4xl   dark:bg-[#333333]'>
-      <div className='flex justify-between items-center'>
-        <UserPanelSearch width={'md:w-[320px]'} handleSearch={handleSearch}/>  
-        <UserPanelFilter/>
+    <div className='flex flex-col gap-10 h-[84%] mt-4 p-8 bg-[#F3F4F6] rounded-4xl   dark:bg-[#333333]'>
+      <div className='flex flex-col gap-4 md:gap-0 md:flex-row md:justify-between items-center'>
+        <UserPanelSearch width={'md:w-[320px]'} handleSearch={handleSearch} />
+
+        <div
+          className="flex h-full items-center bg-[#ffff] dark:bg-[#454545] dark:text-[#ffff]
+          rounded-xl border shadow p-2 md:p-1 border-[#EAEAEA] "
+        >
+          <span className="text-[16px] hidden md:inline ps-3">
+            {t("coursesPayment.filters")}
+          </span>
+          <select
+            value={filterOption}
+            onChange={(e) => {
+              setFilterOption(e.target.value);
+              setCurrentPage(1);
+            }}
+            className=" rounded-xl text-sm cursor-pointer ps-2 text-gray-600   dark:bg-[#454545] dark:text-[#ffff] bg-[#ffff]">
+            <option value="all">({t("favoriteNews.all")})</option>
+            <option value="false">({t("coursesPayment.AwaitingConfirmation")})</option>
+            <option value="true">({t("coursesPayment.confirmed")})</option>
+          </select>
+        </div>
       </div>
       <div className='flex flex-col justify-between h-[440px] p-6 bg-[#FFFFFF] rounded-2xl   dark:bg-[#454545]'>
         <div className='flex flex-col gap-4'>
-          <UserPanelTitle 
-          titleData={{
-            title1: t('myNewsComments.title1'), justify1: 'justify-start', w1: 'w-60',
-            title2: t('myNewsComments.title2'), justify2: 'justify-start', w2: 'w-52',
-            title3: t('myNewsComments.title3'), justify3: 'justify-start', w3: 'w-52',
-            title4: t('myNewsComments.title4'), justify4: 'justify-center', w4: 'w-28',
-            title5: t('myNewsComments.title5'), justify5: 'justify-center', w5: 'w-30',
-            title6: t('myNewsComments.title6'), justify6: 'justify-center', w6: 'w-24',
-          }}/>
-          <div className='max-h-32 overflow-y-scroll
-          lg:max-h-66'>
-            <div className='flex flex-col gap-4
-            md:gap-0'>
+          <UserPanelTitle
+            titleData={{
+              title1: t('myNewsComments.title1'), justify1: 'justify-start', w1: 'w-60',
+              title2: t('myNewsComments.title2'), justify2: 'justify-start', w2: 'w-52',
+              title3: t('myNewsComments.title3'), justify3: 'justify-start', w3: 'w-52',
+              title4: t('myNewsComments.title4'), justify4: 'justify-center', w4: 'w-28',
+              title5: t('myNewsComments.title5'), justify5: 'justify-center', w5: 'w-30',
+              title6: t('myNewsComments.title6'), justify6: 'justify-center', w6: 'w-24',
+            }} />
+          <div className='max-h-66 overflow-y-scroll'>
+            <div className='flex flex-col gap-4 md:gap-0'>
               {
-                myNewsCommentsData?.myNewsCommetDtos?.map((item , index) => {
-                  return <MyNewsComment item={item} key={index}/>
-                })
+                isLoading ? (
+                  [...Array(commentsPerPage)].map((_, index) => (
+                    <FavoritesSkeleton key={index + 2} />
+                  ))
+                ) : currentComments.length > 0 ? (
+                  currentComments.map((item, index) => {
+                    return <MyNewsComment item={item} key={index} />
+                  })
+                ) : (
+                  <div className="w-full">
+                    <Lottie
+                      className="w-[200px] h-[170px] my-4 mx-auto"
+                      animationData={empty}
+                      loop={true}
+                    />
+                    <p className="font-semibold text-[black] text-[20px] text-center dark:text-[#848484]">
+                      {t("navbar.notfound")}
+                    </p>
+                  </div>
+                )
               }
             </div>
           </div>
         </div>
+
         <div className='flex justify-between items-center'>
           <ReactPaginate
             breakLabel="..."
             nextLabel=" >"
             previousLabel="< "
-            // onPageChange={handlePageChange}
-            // pageCount={pageSize}
+            onPageChange={handlePageChange}
+            pageCount={totalPages}
             marginPagesDisplayed={1}
             pageRangeDisplayed={3}
-            // forcePage={currentPage}
+            forcePage={currentPage - 1}
             containerClassName="flex flex-wrap justify-center gap-1 sm:gap-2"
             pageClassName="px-3 py-2 sm:px-5 sm:py-3 rounded-[15px] font-semibold shadow-md cursor-pointer text-sm sm:text-xl bg-[#EAEAEA] dark:bg-[#555] text-black dark:text-[#fff]"
             activeClassName="!bg-[#008C78] text-white rounded-2xl shadow-md"
@@ -75,11 +144,27 @@ const MyNewsComments = () => {
             previousLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
             nextLinkClassName="font-bold text-lg sm:text-2xl px-1 sm:px-2 py-1 flex items-center justify-center h-full w-full"
           />
-          <UserPanelShowNumber/>
+          <div className="flex items-center dark:bg-[#454545] dark:text-[#ffff] rounded-xl border shadow-md p-1 border-[#EAEAEA] ">
+            <span className="text-[16px] hidden md:inline">
+              {t("favoriteNews.NumberShows")}
+            </span>
+            <select
+              value={commentsPerPage}
+              onChange={(e) => {
+                setCommentsPerPage(Number(e.target.value));
+                setCurrentPage(1);
+              }}
+              className=" rounded-xl text-sm cursor-pointer px-3 py-1 dark:bg-[#454545] dark:text-[#ffff]"
+            >
+              <option value={2}>2</option>
+              <option value={4}>4</option>
+              <option value={6}>6</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
   )
 }
 
-export default MyNewsComments
+export default MyNewsComments;
