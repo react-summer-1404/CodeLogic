373de85035e3f393775e1d1be0c7399e2a15cc30
Parsa <mparsabahredar@gmail.com>
2025-11-19 12:23:18 +0330

    C

---
+++ src/components/course/CommentReplyModal/CommentReplyModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentReplyModal = () => {
+  return (
+    <div className='flex p-4 rounded-xl bg-[#FFFFFF] fixed top-40 right-176 z-50'>
+
+    </div>
+  )
+}
+
+export default CommentReplyModal
--- src/components/course/CourseComment/CourseComment.jsx
+++ src/components/course/CourseComment/CourseComment.jsx
@@ -6,6 +6,7 @@ import {likeCourseComments} from '../../../core/services/api/post/likeCourseComm
 import {disLikeCourseComments} from '../../../core/services/api/post/disLikeCourseComments'
 import { useTranslation } from 'react-i18next'
 import CourseCommentForm from '../CourseCommentForm/CourseCommentForm'
+import CommentReplyModal from '../commentReplyModal/CommentReplyModal'
 
 
 const CourseComment = ({item}) => {
@@ -54,55 +55,72 @@ const CourseComment = ({item}) => {
         setIsOpen(value)
     }
 
+    const [showReplies, setShowReplies] = useState(false)
+    const onToggleShowReplies = (value) => {
+        setShowReplies(value)
+    }
 
 
   return (
-    <div>
-        <div className='flex items-end gap-4'>
-            <img src={CourseCommentImg}/>
-            <div className='flex flex-col gap-1'>
-                <h4 className='font-bold text-base text-[#1E1E1E]'>{item.commentUserName}</h4>
-                <span className='font-regular text-sm text-[#848484]'>{item.commentDate}</span>
+    <>
+        <div>
+            <div className='flex items-end gap-4'>
+                <img src={CourseCommentImg}/>
+                <div className='flex flex-col gap-1'>
+                    <h4 className='font-bold text-base text-[#1E1E1E]'>{item.commentUserName}</h4>
+                    <span className='font-regular text-sm text-[#848484]'>{item.commentDate}</span>
+                </div>
             </div>
-        </div>
-        <div className='flex flex-col gap-2 mt-[10px]'>
-            <span className='font-regular text-sm text-[#1E1E1E]'>{item.commentTitle}</span>
-            <span className='font-regualr text-sm text-[#848484]'>{item.commentDescribe}</span>
-        </div>
-        <div className='flex items-center gap-6 mt-4'>
-            <button className='flex gap-1 text-[#1E1E1E]'>
-                <Comment/>
-                <span onClick={() => {onToggleReply(false)}} className='font-regular text-xs cursor-pointer'>
-                    {t('courseComment.closeReplyBtn')}
-                </span>
-            </button>
-            <button 
-            onClick={onLike}
-            className='flex gap-1 text-[#1E1E1E] cursor-pointer'>
-                <DisLike/>
-                <span className='font-regular text-xs'>{item.commentLikeNum}</span>
-            </button>
-            <button 
-            onClick={onDisLike}
-            className='flex gap-1 text-[#1E1E1E] cursor-pointer'>
-                <span className='rotate-180'>
+            <div className='flex flex-col gap-2 mt-[10px]'>
+                <span className='font-regular text-sm text-[#1E1E1E]'>{item.commentTitle}</span>
+                <span className='font-regualr text-sm text-[#848484]'>{item.commentDescribe}</span>
+            </div>
+            <div className='flex items-center gap-6 mt-4'>
+                <button className='flex gap-1 text-[#1E1E1E]'>
+                    <Comment/>
+                    <span onClick={() => {onToggleReply(false)}} className='font-regular text-xs cursor-pointer'>
+                        {t('courseComment.closeReplyBtn')}
+                    </span>
+                </button>
+                <button 
+                onClick={onLike}
+                className='flex gap-1 text-[#1E1E1E] cursor-pointer'>
                     <DisLike/>
-                </span>
-                <span className='font-regular text-xs'>{item.commentDisLikeNum}</span>
-            </button>
-            <button onClick={() => {onToggleReply(true)}} className='font-regular text-xs text-[#008C78] cursor-pointer'>
-                {t('courseComment.openReplyBtn')}
-            </button>
+                    <span className='font-regular text-xs'>{item.commentLikeNum}</span>
+                </button>
+                <button 
+                onClick={onDisLike}
+                className='flex gap-1 text-[#1E1E1E] cursor-pointer'>
+                    <span className='rotate-180'>
+                        <DisLike/>
+                    </span>
+                    <span className='font-regular text-xs'>{item.commentDisLikeNum}</span>
+                </button>
+                <button 
+                onClick={() => {onToggleShowReplies(true)}}
+                className='font-regular text-xs text-[#1E1E1E] cursor-pointer'>
+                    {t('courseComment.replyNumber')}
+                </button>
+                <button onClick={() => {onToggleReply(true)}} className='font-regular text-xs text-[#008C78] cursor-pointer'>
+                     {t('courseComment.openReplyBtn')}
+                </button>
+            </div>
+            {isOpen 
+            && 
+            <div className='mt-8'>
+                <CourseCommentForm 
+                titlePlaceholder={t('courseCommentReply.titleInputsPlcholder')}
+                textPlaceholder={t('courseCommentReply.textInputsPlcholder')}/>
+            </div>
+            }
         </div>
-        {isOpen 
-        && 
-        <div className='mt-8'>
-            <CourseCommentForm 
-            titlePlaceholder={t('courseCommentReply.titleInputsPlcholder')}
-            textPlaceholder={t('courseCommentReply.textInputsPlcholder')}/>
-        </div>
-        }
-    </div>
+        {showReplies && (
+            <>
+                <div onClick={() => onToggleShowReplies(false)} className='fixed inset-0 bg-[#1E1E1E] opacity-50 z-40'/>                
+                <CommentReplyModal />
+            </>
+        )}
+    </>
   )
 }
 
