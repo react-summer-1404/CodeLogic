d05f31dd94b9f890025860b3baa0f635f077f320
Abolfazl <abolfazlmnf83@gmail.com>
2025-11-23 02:08:44 +0330

    my courses user panel binded

--- src/components/userpanel/MyCourse/MyCourse.jsx
+++ src/components/userpanel/MyCourse/MyCourse.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Eye from '../../../assets/Icons/Eye'
-import Receipt from '../../../assets/Icons/Receipt'
-import ReactCourseImg from '../../../assets/Images/A/courses/2.png'
-import { motion } from 'framer-motion'
+import React from "react";
+import Eye from "../../../assets/Icons/Eye";
+import Receipt from "../../../assets/Icons/Receipt";
+import ReactCourseImg from "../../../assets/Images/A/courses/2.png";
+import { motion } from "framer-motion";
+import AddCardIcon from "@mui/icons-material/AddCard";
 
+const textClass =
+  "font-regular text-base text-[#1E1E1E] truncate  dark:text-[#DDDDDD]";
 
-const textClass = 'font-regular text-base text-[#1E1E1E]   dark:text-[#DDDDDD]'
-
-const MyCourse = ({item}) => {
-
+const MyCourse = ({ item }) => {
   const Animate = {
     hidden: { opacity: 0, y: -20 },
     visible: {
@@ -18,37 +18,61 @@ const MyCourse = ({item}) => {
     },
   };
 
-
   return (
-    <motion.div 
-    variants={Animate}
-    initial="hidden"
-    animate="visible"
-    className='flex items-center py-[14px] border-t border-b border-[#EAEAEA]'>
-      <div className='flex items-center gap-4 w-64'>
-        <img src={ReactCourseImg}/>
+    <motion.div
+      variants={Animate}
+      initial="hidden"
+      animate="visible"
+      className="flex items-center py-[14px] border-t border-b border-[#EAEAEA]"
+    >
+      <div className="flex items-center gap-4 w-64">
+        <img
+          className="w-[28px] h-[28px] rounded-full object-cover"
+          src={item.tumbImageAddress}
+        />
         <div>
-          <span className={textClass}>آموزش ری اکت</span>
+          <span className={textClass}>{item.course.title}</span>
         </div>
       </div>
-      <div className='flex justify-center w-50'>
-        <span className={textClass}>دکتر بحرالعلوم</span>
+      <div className="flex justify-center w-50">
+        <span
+          className={textClass}
+        >{`${item.course.teacher.fName}  ${item.course.teacher.lName}`}</span>
       </div>
-      <div className='flex justify-center w-30'>
-        <span className='py-[2px] px-[10px] font-regular text-base text-[#008C78] bg-[#EEFFFC] rounded-[8px]'>پرداخت شده</span>
+      <div className="flex justify-center w-30">
+        <span
+          className={`py-[2px] px-[10px] font-regular text-base 
+          ${
+            item.paymentStatus === "پرداخت شده"
+              ? " text-[#008C78] bg-[#EEFFFC]"
+              : "bg-[#FFECEC] text-[#E7000B] "
+          } rounded-[8px]`}
+        >
+          {item.paymentStatus}
+        </span>
       </div>
-      <div className='flex justify-center w-36'>
-        <span className={textClass}>9000,000</span>
+      <div className="flex justify-center w-36">
+        <span className={textClass}>{item.cost.toLocaleString()}</span>
       </div>
-      <div className='flex justify-center w-34'>
-        <span className={textClass}>1404/09/09</span>
+      <div className="flex justify-center w-34 ">
+        <span className={textClass}>{item.lastUpdate}</span>
       </div>
-      <div className='flex justify-center gap-4 w-32'>
-        <Eye/>
-        <Receipt/>
+      <div className="flex justify-center items-center gap-4 w-32 ">
+        <div>
+          <Eye />
+        </div>
+        {item.paymentStatus === "پرداخت نشده" ? (
+          <div>
+            <AddCardIcon className="text-[#008C78] cursor-pointer" />
+          </div>
+        ) : (
+          <div>
+            <Receipt />
+          </div>
+        )}
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default MyCourse
+export default MyCourse;
--- src/pages/UserPanel/MyCourses/MyCourses.jsx
+++ src/pages/UserPanel/MyCourses/MyCourses.jsx
@@ -1,91 +1,124 @@
-import React, { useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import UserPanelSearch from '../../../components/common/UserPanelSearch/UserPanelSearch'
-import UserPanelTitle from '../../../components/common/UserPanelTitle/UserPanelTitle' 
-import MyCourse from '../../../components/userPanel/MyCourse/MyCourse'
-import ReactPaginate from 'react-paginate'
-import { useTranslation } from 'react-i18next'
-import GetMyCourses from '../../../core/services/api/get/GetMyCourses'
-import Lottie from "lottie-react"
-import empty from "../../../assets/Images/empty.json"
-import FavoritesSkeleton from "../../../components/common/skeleton/favorites/FavoritesSkeleton"
-
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import UserPanelSearch from "../../../components/common/UserPanelSearch/UserPanelSearch";
+import UserPanelTitle from "../../../components/common/UserPanelTitle/UserPanelTitle";
+import MyCourse from "../../../components/userPanel/MyCourse/MyCourse";
+import ReactPaginate from "react-paginate";
+import { useTranslation } from "react-i18next";
+import GetMyCourses from "../../../core/services/api/get/GetMyCourses";
+import Lottie from "lottie-react";
+import empty from "../../../assets/Images/empty.json";
+import FavoritesSkeleton from "../../../components/common/skeleton/favorites/FavoritesSkeleton";
 
 const MyCourses = () => {
-
   const [currentPage, setCurrentPage] = useState(1);
   const [coursesPerPage, setCoursesPerPage] = useState(2);
-  const [searchQuery , setSearchQuery] = useState('')
-  
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filterOption, setFilterOption] = useState("");
+
   const handleSearch = (searchTerm) => {
-    setSearchQuery(searchTerm)
+    setSearchQuery(searchTerm);
     setCurrentPage(1);
-  }
+  };
 
   const { data: myCoursesData, isLoading } = useQuery({
-    queryKey: ['GETMYCOURSES', searchQuery],
-    queryFn: () => GetMyCourses({
-      Query: searchQuery,
-      SortType: 'accept'
-    })
-  })
+    queryKey: ["GETMYCOURSES", searchQuery, currentPage, coursesPerPage],
+    queryFn: () =>
+      GetMyCourses({
+        Query: searchQuery,
+        SortType: filterOption,
+        RowsOfPage: coursesPerPage,
+        PageNumber: currentPage,
+      }),
+  });
 
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   const allCourses = myCoursesData?.listOfMyCourses || [];
-  
+  const totalCount = myCoursesData?.totalCount || 0;
+
+  const totalPages = Math.ceil(totalCount / coursesPerPage);
   const startIndex = (currentPage - 1) * coursesPerPage;
   const endIndex = startIndex + coursesPerPage;
-  const currentCourses = allCourses.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(allCourses.length / coursesPerPage);
-
+  const paginatedCourses = allCourses.slice(startIndex, endIndex);
   const handlePageChange = (p) => {
     const selectedPage = p.selected + 1;
     setCurrentPage(selectedPage);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-
   return (
-    <div className='flex flex-col gap-10 h-[85%] p-8 bg-[#F3F4F6] rounded-4xl   dark:bg-[#333333]'>
-      <div className='flex justify-between items-center'>
-        <UserPanelSearch width={'w-[439px]'} handleSearch={handleSearch}/>  
-      </div>
-      <div className='flex flex-col justify-between h-[440px] p-6 bg-[#FFFFFF] rounded-2xl   dark:bg-[#454545]'>
-        <div className='flex flex-col gap-4 max-h-96 overflow-y-scroll'>
-          <UserPanelTitle 
-          titleData={{
-            title1: t('myCourses.title1'), justify1: 'justify-start', w1: 'w-64',
-            title2: t('myCourses.title2'), justify2: 'justify-center', w2: 'w-50',
-            title3: t('myCourses.title3'), justify3: 'justify-center', w3: 'w-30',
-            title4: t('myCourses.title4'), justify4: 'justify-center', w4: 'w-36',
-            title5: t('myCourses.title5'), justify5: 'justify-center', w5: 'w-34',
-            title6: t('myCourses.title6'), justify6: 'justify-center', w6: 'w-32'
-          }}/>
-          {
-            isLoading ? (
-              [...Array(coursesPerPage)].map((_, index) => (
-                <FavoritesSkeleton key={index + 2} />
-              ))
-            ) : currentCourses.length > 0 ? (
-              currentCourses.map((item , index) => {
-                return <MyCourse item={item} key={index}/>
-              })
-            ) : (
-              <div className="w-full">
-                <Lottie
-                  className="w-[200px] h-[170px] my-4 mx-auto"
-                  animationData={empty}
-                  loop={true}
-                />
-                <p className="font-semibold text-[black] text-[20px] text-center dark:text-[#848484]">
-                  {t("navbar.notfound")}
-                </p>
-              </div>
-            )
-          }
+    <div className="flex flex-col gap-10 h-[85%] p-8 bg-[#F3F4F6] rounded-4xl   dark:bg-[#333333]">
+      <div className="flex flex-col gap-4 md:gap-0 md:flex-row md:justify-between items-center">
+        <UserPanelSearch width={"md:w-[439px]"} handleSearch={handleSearch} />
+        <div
+          className="flex h-full items-center bg-[#ffff] dark:bg-[#454545] dark:text-[#ffff]
+         rounded-xl border shadow p-2 md:p-1 border-[#EAEAEA] "
+        >
+          <span className="text-[16px] hidden md:inline ps-3">
+            {t("coursesPayment.filters")}
+          </span>
+          <select
+            value={filterOption}
+            onChange={(e) => {
+              setFilterOption(e.target.value);
+              setCurrentPage(1);
+            }}
+            className="rounded-xl text-sm cursor-pointer  ps-2 text-gray-600   dark:bg-[#454545] dark:text-[#ffff] bg-[#ffff]"
+          >
+            <option value="">({t("favoriteNews.all")})</option>
+            <option value="LastUpdate">
+              ({t("favoriteCourses.lastUpdate")})
+            </option>
+          </select>
         </div>
-        <div className='flex justify-between items-center'>
+      </div>
+      <div className="flex flex-col justify-between h-[440px] p-6 bg-[#FFFFFF] rounded-2xl   dark:bg-[#454545]">
+        <div className="flex flex-col gap-4 max-h-96 overflow-y-scroll">
+          <UserPanelTitle
+            titleData={{
+              title1: t("myCourses.title1"),
+              justify1: "justify-start",
+              w1: "w-64",
+              title2: t("myCourses.title2"),
+              justify2: "justify-center",
+              w2: "w-50",
+              title3: t("myCourses.title3"),
+              justify3: "justify-center",
+              w3: "w-30",
+              title4: t("myCourses.title4"),
+              justify4: "justify-center",
+              w4: "w-36",
+              title5: t("myCourses.title5"),
+              justify5: "justify-center",
+              w5: "w-34",
+              title6: t("myCourses.title6"),
+              justify6: "justify-center",
+              w6: "w-32",
+            }}
+          />
+          {isLoading ? (
+            [...Array(coursesPerPage)].map((_, index) => (
+              <FavoritesSkeleton key={index + 2} />
+            ))
+          ) : totalCount > 0 ? (
+            paginatedCourses.map((item, index) => {
+              return <MyCourse item={item} key={index} />;
+            })
+          ) : (
+            <div className="w-full">
+              <Lottie
+                className="w-[200px] h-[170px] my-4 mx-auto"
+                animationData={empty}
+                loop={true}
+              />
+              <p className="font-semibold text-[black] text-[20px] text-center dark:text-[#848484]">
+                {t("navbar.notfound")}
+              </p>
+            </div>
+          )}
+        </div>
+        <div className="flex justify-between items-center">
           <ReactPaginate
             breakLabel="..."
             nextLabel=" >"
@@ -123,7 +156,7 @@ const MyCourses = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MyCourses;
