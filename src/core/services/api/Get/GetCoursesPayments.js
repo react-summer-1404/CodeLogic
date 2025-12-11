b27560ee8a91d391d2e8c170ac586fcbde41b88b
Abolfazl <abolfazlmnf83@gmail.com>
2025-12-11 16:43:30 +0330

    c

--- src/core/services/api/Get/GetCoursesPayments.js
+++ src/core/services/api/Get/GetCoursesPayments.js
@@ -2,7 +2,7 @@ import React from "react";
 import http from "../../../interceptor/interceptor.js";
 export const GetCoursesPayments = async () => {
   try {
-    const result = http.get("/CoursePayment");
+    const result = http.get("/CoursePayment/StudentUserPayList");
     console.log(result);
     return result;
   } catch (err) {
